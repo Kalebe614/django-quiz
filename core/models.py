2907a2f5feb4eab23e06618ec62f7a1df3adebe9
kalebe <kalebe613@gmail.com>
2023-08-15 14:30:46 +0100

    Create quiz and the api questions

--- core/models.py
+++ core/models.py
@@ -17,22 +17,38 @@ class BaseModel(models.Model):
 class CategoryModel(BaseModel):
     name = models.CharField(max_length=100, null=False, blank=True)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
+
     def __str__(self):
         return self.name
     
 
 class QuestionModel(BaseModel):
     category = models.ForeignKey(CategoryModel, related_name='questions', on_delete=models.CASCADE)
-    leve_choice = models.IntegerField('Level Choice', choices=LEVEL_CHOICE, null=False, blank=False)
+    level_choice = models.IntegerField('Level Choice', choices=LEVEL_CHOICE, null=False, blank=False)
     question_type = models.CharField('Question Type', max_length=3, choices=QUESTION_TYPE, null=False, blank=False)
     question = models.CharField('Question',max_length=255, null=False, blank=False)
     
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
     def __str__(self):
         return self.question
+
 class AnswerModel(BaseModel):
-    question = models.ForeignKey(QuestionModel, related_name='answer', on_delete=models.CASCADE)
+    question = models.ForeignKey(QuestionModel, related_name='answers', on_delete=models.CASCADE)
     text = models.CharField('Text Answer', max_length=255)
     is_correct = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name = 'Answer'
+        verbose_name_plural = 'Answers'
+
+
     def __str__(self):
         return self.text
+
+        
