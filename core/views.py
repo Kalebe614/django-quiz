d13ede8a8a331f155d2d4958c609e4b5b9fa742f
kalebe <kalebe613@gmail.com>
2023-09-06 17:30:43 +0100

    -ã

--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
 from django.views.generic import ListView, CreateView
-from .models import QuestionModel, QuizModel, AnswerModel
+from .models import QuestionModel, QuizModel, AnswerModel, CategoryModel
 from django.db.models import Prefetch
 from .utils import reset_quiz_data
 class IndexView(ListView):
     template_name = 'index.html'
-    context_object_name = 'quiz'
-    model = QuestionModel
+    context_object_name = 'categories'
+    model = CategoryModel
 
 
 class QuizView(ListView):
@@ -15,14 +15,28 @@ class QuizView(ListView):
   
     def get_queryset(self):
 
-        reset_quiz_data(self, QuizModel)
+        reset_quiz_data(self, QuizModel)#reset the temporary quiz data
 
-        queryset = super().get_queryset()
+        queryset = super().get_queryset() 
         
-        queryset = QuestionModel.objects.all().prefetch_related(
-            Prefetch('answer_question', queryset=AnswerModel.objects.all().order_by('?'))
-        )
-        return queryset
+        category = self.request.GET.get('category')# Get the selected category 
+
+        # Category 0 represents all categories
+        if category == "0":
+            queryset = QuestionModel.objects.all().prefetch_related(
+                Prefetch('answer_question', queryset=AnswerModel.objects.all().order_by('?')))
+        else:
+            queryset = QuestionModel.objects.filter(category=category).prefetch_related(
+                Prefetch('answer_question', queryset=AnswerModel.objects.all().order_by('?')))
+        
+        # If an non-existent number was forced, get all questions
+        if not queryset.exists():
+             queryset = QuestionModel.objects.all().prefetch_related(
+                Prefetch('answer_question', queryset=AnswerModel.objects.all().order_by('?')))
+
+        return queryset[:30] # Limit to 30 questions
+    
+
 
 class ResultView(ListView):
     template_name = 'result.html'
@@ -33,14 +47,20 @@ class ResultView(ListView):
         
         context = super().get_context_data(**kwargs)
 
+        #Get the total number of questions answered correctly
         total_correct_answers = QuizModel.objects.filter(is_correct=True).count()  
+        
+        #Get the total number of questions
         total_questions = QuizModel.objects.all().count()
+
+        #Get the total time taken to complete the quiz
         time = QuizModel.objects.last()
     
-
+        #Create all context objects
         context['total_correct_answers'] = total_correct_answers
         context['score'] =  round((total_correct_answers / total_questions)*100,1)
         context['total_time'] = time.total_time
+        
         return context
 
 
